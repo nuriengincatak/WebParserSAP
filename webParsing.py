2ddf6b767168edaac3b6a99620bcc2b5425f0985
Your Name <you@example.com>
2017-11-20 15:47:01 +0100

    testing

--- webParsing.py
+++ webParsing.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import requests, time
 import xlwt
-import subprocess
+import datetime
 from dateutil import parser
-from joblister.word_checker import word_checker
+from practice_python.german_pie import germanpie
 
 wb = xlwt.Workbook()
 sheet = wb.add_sheet("New Sheet", cell_overwrite_ok=True)
@@ -13,10 +13,10 @@ links = []
 german = []
 dates = []
 jobID = []
-print("            -- Job Lister--\nOne page takes around 30 seconds\nSAP.xls file will be created on the same folder")
+print("          --SAP Job Lister--\nOne page takes around 30 seconds\nSAP.xls file will be created on the same folder")
 print("Your sheet will have Name + date + Ad ID + link and german requirements" )
 number = int(input("How many pages would you like to have? \n"))
-keyword_main = str(input("What is the main keyword you would like to have? \n"))
+keyword_main = "Working student"      #  str(input("What is the main keyword you would like to have? \n"))
 keyword = str(input("What is the keyword you would like to have? (case-sensitive)\n"))
 print("Please wait %d seconds" % (number*30))
 
@@ -25,8 +25,11 @@ for x in range(0, number):
     url = rawurl + str(x*25)
     r=requests.get(url)
 
+    #r_html = r.text
+
     soup = BeautifulSoup(r.content, "lxml")
 
+    #print(soup.find_all(name="span"))
     for link in soup.find_all(class_="jobTitle-link"):
         text.append(link.text) #name of the ad
         fullLinks = "https://jobs.sap.com" + link['href']
@@ -39,17 +42,29 @@ for x in range(0, number):
 
         str1 = parser.parse(dateList[j].text).date()
         dates.append(str1)
+        # print(dateList[j].text)
 
-
+ger = germanpie
 for y in range(0, len(links)):
     time.sleep(0.5)
-    german.append(word_checker(word_checker, links[y], keyword))  #searching for the keyword in each ad
+    german.append(ger.german_checker(ger, links[y], keyword))  #searching for the keyword in each ad
 
  #to eliminate the first empty one
 
-length = len(links)
 
+length = len(links)
+"""
+for i in range(0, length):
+    print(text[i])
+    print(links[i])
+    time.sleep(1) #waits 2 secons to prevent the server from suspending
+    print(german_checker(links[i]))
+"""
 print("total number of jobs that has been processed is: " + str(len(links)))
+
+
+
+#todo add the job ads' ID number
 
 for row_index in range(0, len(text)):
     sheet.write(row_index, 0, text[row_index])
@@ -60,6 +75,6 @@ for row_index in range(0, len(text)):
 
 wb.save("SAP.xls")
 
-subprocess.call(["C:\Program Files (x86)\Microsoft Office\Office15\EXCEL.EXE", 'SAP.xls'])
+
 
 
